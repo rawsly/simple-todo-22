5a190cafd5ed8ce69f9939aacccba650e72c3b43
Ahmet Özdemir <ahmet.ozdemir@obss.com.tr>
2022-01-26 20:08:26 +0300

    project finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from './App.module.css';
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  const handleAdd = () => {
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
+
+  const handleChange = (event) => {
+    const { target } = event;
+    const { value } = target;
+    setTodo(value);
+  }
+
+  const handleDelete = (index) => {
+    const newTodos = todos.filter((t, currentIndex) => currentIndex !== index);
+    console.log(newTodos);
+    setTodos(newTodos);
+  }
+
+  console.log("todos:", todos);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <div className={styles.inputContainer}>
+        <input name="todo" className={styles.todoInput} onChange={handleChange} value={todo} />
+        <button className={styles.btn} onClick={handleAdd}>Add</button>
+      </div>
+      <div>TODO</div>
+      {todos.map((t, index) => (
+        <div key={`${t}_${index}`} className={styles.todos}>
+          <p>{t}</p>
+          <button onClick={() => handleDelete(index)}>X</button>
+        </div>
+      ))}
     </div>
   );
 }
